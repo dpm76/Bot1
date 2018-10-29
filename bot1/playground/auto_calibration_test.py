b3b6155e1fef8e796e5952b19e816198991a142a
dpm76 <davidpm.itengineer@gmail.com>
2018-10-29 19:48:23 +0100

    auto calibration test with several passes

--- bot1/playground/auto_calibration_test.py
+++ bot1/playground/auto_calibration_test.py
@@ -9,12 +9,37 @@ from sensor.imu6050 import Imu6050
 from time import sleep
 
 MAX_THROTTLE = 30.0
-INC_THROTTLE = 5.0
+THROTTLE_STEP = 5.0
+
+def callibrate(motor, imu, throttleStep):
+
+    throttle = 0.0
+    angSpeed = 0.0
+    
+    motor.setNeutralThrottle()
+
+    while abs(throttle) < MAX_THROTTLE and angSpeed < 1.0:
+    
+        throttle += throttleStep
+        print("Throttle = {0}".format(throttle))
+        motor.setThrottle(throttle)
+        sleep(0.1)
+        angSpeed = imu.readAngSpeedZ()
+    
+    motor.setNeutralThrottle()
+    if abs(throttle) >= MAX_THROTTLE:
+        
+        raise Exception("The motor get no motion!")
+    
+    return throttle
+        
 
 motor = Motor(0)
 imu = Imu6050()
 
+print("Init IMU")
 imu.start()
+print("Init motor")
 motor.start()
 
 throttle = 0.0
@@ -24,21 +49,25 @@ print("Start motor callibration")
 
 try:
 
-    while throttle < MAX_THROTTLE and angSpeed < 1.0:
+    print("fordwards")
+    accThrottle = 0.0
+    for i in range(3):
+        throttle = callibrate(motor, imu, THROTTLE_STEP)
+        print("pass {0}: min throttle = {1}".format(i, throttle))
+        accThrottle += throttle
+        
+    avgThrottle = accThrottle / i
+    print("avg throttle = {0}".format(avgThrottle))
     
-        throttle += INC_THROTTLE
-        print("Throttle = {0}".format(throttle))
-        motor.setThrottle(throttle)
-        sleep(0.1)
-        angSpeed = imu.readAngSpeedZ()
-    
-    if throttle < MAX_THROTTLE:
+    print("backwards")
+    accThrottle = 0.0
+    for i in range(3):
+        throttle = callibrate(motor, imu, -THROTTLE_STEP)
+        print("pass {0}: min throttle = {1}".format(i, throttle))
+        accThrottle += throttle
         
-        print("Motion detected with throttle = {0}".format(throttle))
-        
-    else:
-        
-        print("The motor get no motion!")
+    avgThrottle = accThrottle / i
+    print("avg throttle = {0}".format(avgThrottle))
 
 finally:
 
