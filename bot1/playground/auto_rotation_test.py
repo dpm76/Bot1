011e4afe4a7ad996e22f4fb61bf038e21b0e004b
David Pérez <davidpm.itengineer@gmail.com>
2018-10-31 19:32:52 +0100

    auto rotation test for real robot

--- bot1/playground/auto_rotation_test.py
+++ bot1/playground/auto_rotation_test.py
@@ -8,30 +8,69 @@ import logging
 import time
 
 from engine.driver import Driver
-from sensor.imu_dummy import ImuDummy
+#from sensor.imu_dummy import ImuDummy
+from sensor.imu6050 import Imu6050
 
 
 logging.basicConfig(level=logging.DEBUG)
 
-targetAngle = 90.0
+MAX_THROTTLE = 40.0
+MIN_THROTTLE = 20.0
+PK = 0.05
 
-driver = Driver.createForTesting()
-imu = ImuDummy(0.0)
+def turnTo(targetAngle, imu, driver):
 
+    driver.setNeutral()
+    driver.setMode(Driver.MODE_ROTATE)
+    imu.updateGyroTime()
+    currentAngle = imu.readAngleZ()
+    err1 = (targetAngle-currentAngle)%360.0
+    err2 = (currentAngle-targetAngle)%360.0
+    if err1 < err2:
+        err = err1
+    else:
+        err = err2
+    while err > 5.0:
+        currentAngle = imu.readAngleZ()
+        logging.debug("current angle = {0:.3f}°".format(currentAngle))
+        err1 = (targetAngle-currentAngle)%360.0
+        logging.debug("current err1 = {0:.3f}°".format(err1))
+        err2 = (currentAngle-targetAngle)%360.0
+        logging.debug("current err2 = {0:.3f}°".format(err2))
+        if err1 < err2:
+            err = err1
+            direction = - MIN_THROTTLE - (PK * err1)
+        else:
+            err = err2
+            direction = MIN_THROTTLE + (PK * err2)
+            
+        if direction > MAX_THROTTLE:
+            direction = MAX_THROTTLE
+        elif direction < -MAX_THROTTLE:
+            direction = -MAX_THROTTLE
+        logging.debug("direction = {0}".format(direction))
+        driver.setDirection(direction)        
+        time.sleep(0.1)
+        
+    logging.info("Target reached")
+    driver.setNeutral()
+
+
+#driver = Driver.createForTesting()
+#imu = ImuDummy(0.0)
+driver = Driver.createForRobot()
+imu = Imu6050()
+
+logging.info("Init IMU")
 imu.start()
+logging.info("Init driver")
 driver.start()
 
 try:
-    driver.setNeutral()
-    driver.setMode(Driver.MODE_ROTATE)
-    currentAngle = imu.readAngleZ()
-    direction = -30.0 if (targetAngle-currentAngle)%360.0 < 180.0 else 30.0 
-    driver.setDirection(direction)
-    while abs(targetAngle-currentAngle) < 1.0:
-        currentAngle = imu.readAngleZ()
-        time.sleep(0.1)
-        
-    driver.setNeutral()
+    
+    turnTo(45.0, imu, driver)
+    time.sleep(1)
+    turnTo(315.0, imu, driver)
 
 finally:
     driver.stop()
