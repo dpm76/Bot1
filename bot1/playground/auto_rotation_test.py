73c8d42811c39c6a2202f24fc76a016281408321
dpm76 <davidpm.itengineer@gmail.com>
2018-11-01 19:13:32 +0100

    auto rotation: added integral component to PID

--- bot1/playground/auto_rotation_test.py
+++ bot1/playground/auto_rotation_test.py
@@ -15,22 +15,26 @@ from sensor.imu6050 import Imu6050
 logging.basicConfig(level=logging.DEBUG)
 
 MAX_THROTTLE = 40.0
-MIN_THROTTLE = 20.0
+
 PK = 0.05
+PI = 0.01
 
 def turnTo(targetAngle, imu, driver):
 
     driver.setNeutral()
     driver.setMode(Driver.MODE_ROTATE)
     imu.updateGyroTime()
+    lastTime = time.time()
     currentAngle = imu.readAngleZ()
     err1 = (targetAngle-currentAngle)%360.0
     err2 = (currentAngle-targetAngle)%360.0
+    integral = 0.0
     if err1 < err2:
         err = err1
     else:
         err = err2
     while err > 5.0:
+        currentTime = time.time()
         currentAngle = imu.readAngleZ()
         logging.debug("current angle = {0:.3f}°".format(currentAngle))
         err1 = (targetAngle-currentAngle)%360.0
@@ -38,11 +42,14 @@ def turnTo(targetAngle, imu, driver):
         err2 = (currentAngle-targetAngle)%360.0
         logging.debug("current err2 = {0:.3f}°".format(err2))
         if err1 < err2:
-            err = err1
-            direction = - MIN_THROTTLE - (PK * err1)
+            err = -err1
         else:
             err = err2
-            direction = MIN_THROTTLE + (PK * err2)
+
+        dt = currentTime - lastTime
+        integral += err * dt
+        direction = (PK * err) + (PI * integral)
+        lastTime = currentTime
             
         if direction > MAX_THROTTLE:
             direction = MAX_THROTTLE
